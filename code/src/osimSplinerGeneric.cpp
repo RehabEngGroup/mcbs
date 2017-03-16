4f59f0fdb00db1e6f6d50b144845b595f171ce88
Elena Ceseracciu <eowyn83@gmail.com>
2017-03-16 21:20:10 +0100

    Add license file and missing license header

--- code/src/osimSplinerGeneric.cpp
+++ code/src/osimSplinerGeneric.cpp
@@ -1,4 +1,22 @@
-
+/*
+ * Copyright (C) 2013 C. Pizzolato, M. Reggiani, M. Sartori
+ * Copyright (C) 2015 E.Ceseracciu
+ *
+ * This file is part of Multidimensional Cubic Bspline Library (MCBS).
+ *
+ * MCBS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MCBS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MCBS.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <memory>
 using std::shared_ptr;
